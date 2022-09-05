3f008c8ef9c17a4e3c0db7fce058af2c3657bb61
AlinaF <afassahova9@gmail.com>
2022-09-05 21:06:41 +0500

    add reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int currentAccount = 100;
+        int refill = 100;
+        int bonus = refill / 100;
+        int total;
+
+        if (refill >= 1000) {
+            total = currentAccount + bonus + refill;
+        } else {
+            total = currentAccount + refill;
+        }
+        System.out.println(total);
     }
 }
